30198a7575bf3ee6ee87e8529088323253e2a003
Stefano Palma <stefano.palma1@studenti.unimi.it>
2020-10-11 11:00:10 +0200

    Sphere spawning

---
+++ Assets/IceBomb/FloorColliding.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorColliding : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float gravity;
+    public Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(direction * gravity * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Floor"))
+        {
+            Destroy(this.gameObject); //maybe later with a delay/spawning something else
+        }
+    }
+}
---
+++ Assets/IceBomb/SpawnSphere.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSphere : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform hand;
+
+    public float ForceForward = 1000.0f;
+    public float ForceUp = 400.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnSphereMethod()
+    {
+        GameObject sphere = Instantiate(prefab, hand.position, Quaternion.identity);
+        Rigidbody rb = sphere.GetComponent<Rigidbody>();
+
+        rb.AddForce(transform.forward * ForceForward + transform.up * ForceUp);
+    }
+}
