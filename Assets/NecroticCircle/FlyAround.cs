73d3cb47d3bb349c4be494734b49092ee780a3e1
Stefano Palma <stefano.palma1@studenti.unimi.it>
2020-11-21 18:00:06 +0100

    Portal + skull mockup

---
+++ Assets/NecroticCircle/FlyAround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyAround : MonoBehaviour
+{
+    public float speed;
+    public float desiredHeigth;
+    private Vector3 direction;
+    bool started = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector3.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        if(transform.position.y >= desiredHeigth && started)
+        {
+            direction = new Vector3(Random.Range(-2, 2), 1.0f, Random.Range(-2, 2));
+            started = false;
+        }
+    }
+}
