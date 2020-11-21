3a01c9fda9be711d4552ea959de037c368f4ee0f
Stefano Palma <stefano.palma1@studenti.unimi.it>
2020-11-21 12:59:04 +0100

    Necrotic circle begin

---
+++ Assets/NecroticCircle/CircleTiming.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleTiming : MonoBehaviour
+{
+    public string NameProperty;
+    public bool animatingProperty = true;
+    public float startPoint = 0.5f;
+    public float endPoint = -0.5f;
+
+    private float current;
+
+    private Material mat;
+
+    public float step = 0.5f;
+    public ParticleSystem psystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        current = startPoint;
+        mat.SetFloat(NameProperty, current);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(animatingProperty)
+        {
+            current += step * Time.deltaTime;
+            mat.SetFloat(NameProperty, current);
+            if(current < endPoint)
+            {
+                animatingProperty = false;
+                psystem.Play();
+            }
+        }
+    }
+}
