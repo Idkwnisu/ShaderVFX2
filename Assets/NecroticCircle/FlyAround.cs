102e223bc72aefded64f3c817bc889f005fed62f
Stefano Palma <stefano.palma1@studenti.unimi.it>
2020-12-12 13:13:26 +0100

    Skulls

--- Assets/NecroticCircle/FlyAround.cs
+++ Assets/NecroticCircle/FlyAround.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class FlyAround : MonoBehaviour
 {
-    public float speed;
-    public float desiredHeigth;
+    public Vector2 speedR;
+    public Vector2 desiredHeigthR;
+    public Vector2 delayR;
+
+    public Vector2 positionRange;
+
+    private float speed;
+    private float desiredHeigth;
+
     private Vector3 direction;
     bool started = true;
+    bool delayOver = false;
     // Start is called before the first frame update
     void Start()
     {
+        transform.position += new Vector3(Random.Range(positionRange.x, positionRange.y), 0, Random.Range(positionRange.x, positionRange.y));
         direction = Vector3.up;
+        speed = Random.Range(speedR.x, speedR.y);
+        desiredHeigth = Random.Range(desiredHeigthR.x, desiredHeigthR.y);
+        Invoke("EndDelay", Random.Range(delayR.x, delayR.y));
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += direction * speed * Time.deltaTime;
-        if(transform.position.y >= desiredHeigth && started)
+        if (delayOver)
         {
-            direction = new Vector3(Random.Range(-2, 2), 1.0f, Random.Range(-2, 2));
-            started = false;
+            transform.position += direction * speed * Time.deltaTime;
+            if (transform.position.y >= desiredHeigth && started)
+            {
+                direction = new Vector3(Random.Range(-2, 2), 1.0f, Random.Range(-2, 2));
+                transform.LookAt(transform.position + direction - Vector3.up);
+                started = false;
+            }
         }
+    }
+
+    void EndDelay()
+    {
+        delayOver = true;
     }
 }
--- Assets/NecroticCircle/Spawner.cs
+++ Assets/NecroticCircle/Spawner.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject toSpawn;
+    public float delay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("Spawn", delay);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Spawn()
+    {
+        toSpawn.SetActive(true);
     }
 }
